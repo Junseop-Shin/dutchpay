02b33b07dc17056146d73fa72114788c2fd7dc5e
junseop_shin <nuclearbomb6518@gmail.com>
2023-03-14 14:17:22 +0900

    [feat] 그룹 생성 컴포넌트 스타일링 - styled-component를 이용해 스타일링

--- src/components/CenteredOverlayForm.jsx
+++ src/components/CenteredOverlayForm.jsx
@@ -1,10 +1,32 @@
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import { OverlayWrapper } from './shared/OverlayWrapper';
 
-const CenteredOverlayForm = () => {
+const CenteredOverlayForm = ({children}) => {
     return (
-        <div>
-            CenteredOverlayForm
-        </div>
+        <StyledCentralizedContainer>
+            <StyledHeader>Dutch Pay</StyledHeader>
+            <OverlayWrapper>
+                {children}
+            </OverlayWrapper>
+        </StyledCentralizedContainer>
     );
 }
+
+const StyledHeader = styled.h1`
+    font-weight: 200;
+    letter-spacing: 10px;
+`;
+
+const StyledCentralizedContainer = styled(Container)`
+    width: 50vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0px;
+    gap: 10px;
+`;
 
 export default CenteredOverlayForm
