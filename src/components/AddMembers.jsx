58e01f09145c9ea5081e6c1070426a67d750e0da
junseop_shin <nuclearbomb6518@gmail.com>
2023-03-15 22:17:24 +0900

    [bugfix] 삼성 브라우저 환경에서 InputTags 동작하지 않는 문제 수정 - 삼성브라우저 환경의 경우에는 react-bootstrap의 Form.Control을 이용 - 태그 기능 미제공 및 컴마(,)로 이름 구분

--- src/components/AddMembers.jsx
+++ src/components/AddMembers.jsx
@@ -1,23 +1,31 @@
 import CenteredOverlayForm from './shared/CenteredOverlayForm';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { InputTags } from 'react-bootstrap-tagsinput';
 import { groupMembersState } from '../state/groupMembers';
 import { groupNameState } from '../state/groupName';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { Form, useNavigate } from 'react-router-dom';
 import { ROUTES } from 'routes';
 
 const AddMembers = () => {
     const [groupMembers, setGroupMembers] = useRecoilState(groupMembersState);
     const groupName = useRecoilValue(groupNameState);
     const [validated, setValidated] = useState(false);
+    const [groupMembersString, setGroupMembersString] = useState('');
     const navigate = useNavigate();
+
+    const isSamsungBrowser = useCallback(() => {
+        return window.navigator.userAgent.includes('SAMSUNG');
+    }, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         setValidated(true);
         if (groupMembers.length > 0) {
+            navigate(ROUTES.EXPENSE_MAIN);
+        } else if (isSamsungBrowser() && groupMembersString.length > 0) {
+            setGroupMembers(groupMembersString.split(','));
             navigate(ROUTES.EXPENSE_MAIN);
         }
     };
@@ -29,11 +37,20 @@ const AddMembers = () => {
             validated={validated}
             handleSubmit={handleSubmit}
         >
-            <InputTags
-                data-testId='input-member-names'
-                placeholder='이름 간 띄어쓰기'
-                onTags={(value) => setGroupMembers(value.values)}
-            />
+            {/* TODO: InputTags가 동작하지 않는 환경(삼성 브라우저)에서는 컴마(,)로 구분 */}
+            { isSamsungBrowser() ?
+                <Form.Control
+                    data-testId='input-member-names'
+                    placeholder='이름 간 컴마(,)로 구분'
+                    onChange={(event) => setGroupMembersString(event.target.value)}
+                />
+            :
+                <InputTags
+                    data-testId='input-member-names'
+                    placeholder='이름 간 띄어쓰기'
+                    onTags={(value) => setGroupMembers(value.values)}
+                />
+            }
             {validated && groupMembers.length === 0 && 
                 (<StyledErrorMessage>그룹 멤버들의 이름을 입력해주세요.</StyledErrorMessage>)
             }
